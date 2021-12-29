0bd5774d8ccb07d7422af8ce066ad7396f73ef26
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2021-12-29 23:36:19 +0200

    Improve the PEP documentation
    - Also some minor changes to the code structure
    - Fixed a bug where information would not be displayed if no protocol was provided via command line arguments

--- main.py
+++ main.py
@@ -13,21 +13,54 @@ PROTOCOL_MAPPINGS = {
 
 
 def handleTCPPackets(packetSlice):
+    """
+    handleTCPPackets(packetSlice) -> None
+
+    Unpack the TCP header and print the information
+
+    :param packetSlice: list(str)
+    :return: None
+    """
     for key, value in unpack.tcpHeader(packetSlice).items():
         print(key, ':', value, end=' | ')
 
 
 def handleUDPPackets(packetSlice):
+    """
+    handleUDPPackets(packetSlice) -> None
+
+    Unpack the UDP header and print the information
+
+    :param packetSlice: list(str)
+    :return: None
+    """
     for key, value in unpack.udpHeader(packetSlice).items():
         print(key, ':', value, end=' | ')
 
 
 def handleICMPPackets(packetSlice):
+    """
+    handleICMPPackets(packetSlice) -> None
+
+    Unpack the ICMP header and print the information
+
+    :param packetSlice: list(str)
+    :return: None
+    """
     for key, value in unpack.icmpHeader(packetSlice).items():
         print(key, ':', value, end=' | ')
 
 
 def handleHTTPPackets(packet, protocol):
+    """
+    handleHTTPPackets(packet, protocol) -> None
+
+    Handle packets for each protocol
+
+    :param packet: list(str)
+    :param protocol: str
+    :return: None
+    """
     if protocol == PROTOCOL_MAPPINGS['TCP']:
         print('\n===>> [ ------------ TCP Header ----------- ] <<===')
         handleTCPPackets(packet[20:40])
@@ -40,6 +73,14 @@ def handleHTTPPackets(packet, protocol):
 
 
 def validateKeywords(keywords):
+    """
+    validateKeywords(keywords) -> boolean
+
+    If a parsed keyword is not in the list of accepted keywords, return False
+
+    :param keywords: list(str)
+    :return: boolean
+    """
     for keyword in keywords:
         if keyword not in KEYWORDS:
             return False
@@ -47,6 +88,16 @@ def validateKeywords(keywords):
 
 
 def validateCommandLineArguments(keywords, values):
+    """
+    validateCommandLineArguments(keywords, values) -> None
+
+    Validate the length of the keywords against the length of the values
+    Check that the parsed keywords are in the list of accepted keywords
+
+    :param keywords: list(str)
+    :param values: list(str)
+    :return: None
+    """
     if len(keywords) != len(values):
         print(
             'Invalid command line arguments: the length of the values does not coincide with the length of the keywords')
@@ -58,12 +109,28 @@ def validateCommandLineArguments(keywords, values):
 
 
 def validateProtocol(protocol):
+    """
+    validateProtocol(protocol) -> boolean
+
+    If the parsed protocol is not in the list of accepted protocols, return False
+
+    :param protocol: str
+    :return: boolean
+    """
     if protocol not in PROTOCOL_MAPPINGS.keys():
         return False
     return True
 
 
 def validateIpAddress(ipAddress):
+    """
+    validateIpAddress(ipAddress) -> boolean
+
+    Use the socket library to check if the ip address is valid
+
+    :param ipAddress: str
+    :return: boolean
+    """
     try:
         socket.inet_aton(ipAddress)
     except socket.error:
@@ -72,15 +139,39 @@ def validateIpAddress(ipAddress):
 
 
 def validatePort(port):
+    """
+    validatePort(port) -> boolean
+
+    Check that the port is numeric
+    Convert the port to an integer
+    Check that the specified port matches the allowed port range
+
+    :param port: str
+    :return: boolean
+    """
     if not port.isnumeric():
         return False
+
     numericPort = int(port)
+
     if numericPort < 1 or numericPort > 65535:
         return False
     return True
 
 
 def validateArgDict(argDict):
+    """
+    validateArgDict(argDict) -> None
+
+    - Protocol validation
+    - Destination ip validation
+    - Destination port validation
+    - Source ip validation
+    - Source port validation
+
+    :param argDict: dict
+    :return: None
+    """
     if 'protocol' in argDict and not validateProtocol(argDict['protocol'].upper()):
         print('Protocol not supported. Accepted values:', list(PROTOCOL_MAPPINGS.keys()))
         sys.exit(3)
@@ -103,25 +194,65 @@ def validateArgDict(argDict):
 
 
 def createArgDictionary(keywords, values):
+    """
+    createArgDictionary(keywords, values) -> dict
+
+    Create and validate the command line arguments dictionary
+    Return the dictionary
+
+    :param keywords: list(str)
+    :param values: list(str)
+    :return: dict
+    """
     argDict = {}
     for index, keyword in enumerate(keywords):
         argDict[keyword] = values[index]
+
     validateArgDict(argDict)
+
     return argDict
 
 
 def parseCommandLineArguments():
-    """Get the command line arguments except the first one (name of the program)"""
+    """
+    parseCommandLineArguments() -> dict
+
+    Get the command line arguments except the first one (name of the program)
+    Extract the keywords from the command line arguments
+    Extract the values from the command line arguments
+    Validate the command line arguments
+    Create the command line arguments dictionary and return it
+
+    :return: dict
+    """
     args = sys.argv[1:]
-    """Extract the keywords from the command line arguments"""
     keywords = list(map(lambda arg: arg.replace('-', ''), args[::2]))
-    """Extract the values from the command line arguments"""
     values = args[1::2]
-    """Validate the command line arguments"""
     validateCommandLineArguments(keywords, values)
-    """Create the command line arguments dictionary and return it"""
     return createArgDictionary(keywords, values)
 
+
+def printInfo(data, packet, ipHeaderDict):
+    """
+    printInfo(data, packet, ipHeaderDict) -> None
+
+    Print the gathered information
+
+    :param data: list(list(str))
+    :param packet: list(str)
+    :param ipHeaderDict: dict
+    :return: None
+    """
+    print('\n\n===>> [ ------------ Ethernet Header----- ] <<===')
+    for key, value in unpack.ethHeader(packet[0:14]).items():
+        print(key, ':', value, end=' | ')
+
+    print('\n===>> [ ------------ IP Header ------------ ] <<===')
+    for key, value in ipHeaderDict.items():
+        print(key, ':', value, end=' | ')
+
+    # Handle packets according to the protocol through which the packet was received
+    handleHTTPPackets(data[0], ipHeaderDict['Protocol'])
 
 def main():
     commandLineArguments = parseCommandLineArguments()
@@ -145,20 +276,18 @@ def main():
         s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
 
     while True:
+        # Receive all data
         data = s.recvfrom(65565)
+        # Get the packet
         packet = data[0]
+        # Unpack the IP header
         ipHeaderDict = unpack.ipHeader(packet[:20])
 
-        if ipHeaderDict['Protocol'] == PROTOCOL_MAPPINGS[commandLineArguments['protocol'].upper()]:
-            print('\n\n===>> [ ------------ Ethernet Header----- ] <<===')
-            for key, value in unpack.ethHeader(packet[0:14]).items():
-                print(key, ':', value, end=' | ')
-
-            print('\n===>> [ ------------ IP Header ------------ ] <<===')
-            for key, value in ipHeaderDict.items():
-                print(key, ':', value, end=' | ')
-
-            handleHTTPPackets(data[0], ipHeaderDict['Protocol'])
+        if 'protocol' in commandLineArguments:
+            if ipHeaderDict['Protocol'] == PROTOCOL_MAPPINGS[commandLineArguments['protocol'].upper()]:
+                printInfo(packet, ipHeaderDict)
+        else:
+            printInfo(packet, ipHeaderDict)
 
 
 if __name__ == "__main__":
--- unpack.py
+++ unpack.py
@@ -3,6 +3,14 @@ import struct
 
 
 def ethHeader(data):
+    """
+    ethHeader(data) -> dictionary
+
+    Unpack and destructure the ETH header data
+
+    :param data: list(list(str))
+    :return: dict
+    """
     destinationMac, sourceMac, ethProtocol = struct.unpack("!6s6sH", data)
     return {
         "Protocol": ethProtocol,
@@ -11,30 +19,21 @@ def ethHeader(data):
     }
 
 
-
-# ICMP HEADER Extraction
-def icmpHeader(data):
-    icmpType, code, checksum = struct.unpack('!BBH', data)
-    return {
-        'ICMP type': icmpType,
-        "Code": code,
-        "Checksum": checksum
-    }
-
-
-# UDP Header Extraction
-def udpHeader(data):
-    sourcePort, destPort, length, checksum = struct.unpack('!HHHH', data)
-    return {
-        "Source port": sourcePort,
-        "Destination port": destPort,
-        "Length": length,
-        "Checksum": checksum
-    }
-
-
 # IP Header Extraction
 def ipHeader(data):
+    """
+    ipHeader(data) -> dictionary
+
+    Unpack the IP header data
+    Destructure the IP header data
+    Convert the source address to string
+    Attempt to get the hostname using the source address
+    Convert the destination address to string
+    Attempt to get the hostname using the destination address
+
+    :param data: list(list(str))
+    :return: dict
+    """
     unpackedData = struct.unpack("!BBHHHBBHBBBBBBBB", data)
     version, tos, totalLength, identification, fragmentOffset, ttl, protocol, headerChecksum, *_ = unpackedData
     sourceAddress = '.'.join(map(str, unpackedData[8:12]))
@@ -58,6 +57,14 @@ def ipHeader(data):
 
 # Tcp Header Extraction
 def tcpHeader(data):
+    """
+    tcpHeader(data) -> dictionary
+
+    Unpack and destructure the TCP header data
+
+    :param data: list(list(str))
+    :return: dict
+    """
     sourcePort, destinationPort, sequenceNumber, acknowledgeNumber, \
     offsetReserved, tcpFlag, window, checksum, urgentPointer = struct.unpack('!HHLLBBHHH', data)
     return {
@@ -73,13 +80,66 @@ def tcpHeader(data):
     }
 
 
-def macFormatter(a):
-    return "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (a[0], a[1], a[2], a[3], a[4], a[5])
+# UDP Header Extraction
+def udpHeader(data):
+    """
+    udpHeader(data) -> dictionary
+
+    Unpack and destructure the UDP header data
+
+    :param data: list(list(str))
+    :return: dict
+    """
+    sourcePort, destPort, length, checksum = struct.unpack('!HHHH', data)
+    return {
+        "Source port": sourcePort,
+        "Destination port": destPort,
+        "Length": length,
+        "Checksum": checksum
+    }
 
 
-def getHost(q):
+# ICMP Header Extraction
+def icmpHeader(data):
+    """
+    icmpHeader(data) -> dictionary
+
+    Unpack and destructure the ICMP header data
+
+    :param data: list(list(str))
+    :return: dict
+    """
+    icmpType, code, checksum = struct.unpack('!BBH', data)
+    return {
+        'ICMP type': icmpType,
+        "Code": code,
+        "Checksum": checksum
+    }
+
+
+def macFormatter(macArray):
+    """
+    macFormatter(macArray) -> string
+
+    Transform the mac address into a more common and readable format
+
+    :param macArray: list(str)
+    :return: str
+    """
+    return "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % tuple(macArray)
+
+
+def getHost(ipAddress):
+    """
+    getHost(ipAddress) -> string
+
+    Attempt to get the hostname using the ip address
+
+    :param ipAddress: str
+    :return: str
+    """
     try:
-        k = socket.gethostbyaddr(q)
-    except:
+        k = socket.gethostbyaddr(ipAddress)
+    except socket.error:
         k = 'Unknown'
     return k
